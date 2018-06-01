a22f0c1c2856a46168d410a8595e164a54ceabc7
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-06-01 16:23:36 +0300

    pprint (not testing yet)

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,17 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as file:
+        parsed = json.load(file)
+    return parsed
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=5, sort_keys=False))
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data(sys.argv[1])
+    pretty_print_json(data)
