51b0b78d772f75983f4d4751ca260c69a97e9675
ivanpobeguts <Ivan.Pobeguc@gmail.com>
2018-06-03 20:53:31 +0300

    Fix encoding

--- pprint_json.py
+++ pprint_json.py
@@ -1,17 +1,16 @@
 import json
-import sys
 
 
 def load_data(filepath):
-    with open(filepath, 'r') as file:
+    with open(filepath, 'r', encoding='utf8') as file:
         parsed = json.load(file)
     return parsed
 
 
 def pretty_print_json(data):
-    print(json.dumps(data, indent=5, sort_keys=False))
+    print(json.dumps(data, indent=5, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    data = load_data(sys.argv[1])
+    data = load_data("example.json")
     pretty_print_json(data)
