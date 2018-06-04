febbc45122872214c4f76c2614465e35c182ca0b
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-06-04 11:35:32 +0300

    Fix variables' names + exception handling

--- pprint_json.py
+++ pprint_json.py
@@ -1,17 +1,29 @@
 import json
 import sys
+import os
 
 
 def load_data(filepath):
-    with open(filepath, 'r', encoding='utf8') as file:
-        parsed = json.load(file)
-    return parsed
+    if os.path.exists(filepath):
+        with open(filepath, 'r', encoding='utf8') as file:
+            try:
+                parsed_json = json.load(file)
+            except:
+                print('Incorrect json file: \'{}\'!'.format(filepath))
+                sys.exit()
+    else:
+        print('File \'{}\' does not exist!'.format(filepath))
+        sys.exit()
+    return parsed_json
 
 
-def pretty_print_json(input_json):
-    print(json.dumps(input_json, indent=5, sort_keys=True, ensure_ascii=False))
+def pretty_print_json(raw_json):
+    print(json.dumps(raw_json, indent=5, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    input_json = load_data(sys.argv[1])
-    pretty_print_json(input_json)
+    if len(sys.argv) > 1:
+        raw_json = load_data(sys.argv[1])
+        pretty_print_json(raw_json)
+    else:
+        print('Path to json file expected!')
