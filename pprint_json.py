a82b99d2f14249bc73034e78b7b0cbd656a27588
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-06-04 17:58:06 +0300

    Fix raw_json name remove backslash remove print from load_data()

--- pprint_json.py
+++ pprint_json.py
@@ -4,16 +4,12 @@ import os
 
 
 def load_data(filepath):
-    parsed_json = None
     if os.path.exists(filepath):
         with open(filepath, 'r', encoding='utf8') as file:
             try:
-                parsed_json = json.load(file)
-            except ValueError as ex:
-                print('{}: Incorrect json file (\'{}\')!'.format(type(ex).__name__, filepath))
-    else:
-        print('File \'{}\' does not exist!'.format(filepath))
-    return parsed_json
+                return json.load(file)
+            except ValueError:
+                return None
 
 
 def pretty_print_json(raw_json):
@@ -22,8 +18,10 @@ def pretty_print_json(raw_json):
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        raw_json = load_data(sys.argv[1])
-        if raw_json is not None:
-            pretty_print_json(raw_json)
+        parsed_json = load_data(sys.argv[1])
+        if parsed_json is not None:
+            pretty_print_json(parsed_json)
+        else:
+            print('ERROR: Check, that file "{}" exists and it is valid json file.'.format(sys.argv[1]))
     else:
         print('Path to json file expected!')
